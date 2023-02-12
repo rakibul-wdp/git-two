db97615f51c60c189189bfebb61cba5b4d3a1491
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-12 12:50:18 +0600

    calculate multiple divisiton and equal

--- js/index.js
+++ js/index.js
@@ -4,6 +4,12 @@ const btn = document.querySelectorAll(".btn");
 for (item of btn) {
   item.addEventListener("click", (e) => {
     btnText = e.target.innerText;
+    if (btnText == "x") {
+      btnText = "*";
+    }
+    if (btnText == "÷") {
+      btnText = "/";
+    }
     screen.value += btnText;
   });
 }
